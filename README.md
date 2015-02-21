af5e4b9048411fcd2907288181ca97d478805f71
Chris Hill <you@example.com>
2015-02-21 10:31:42 +0000

    Show the sunset if it is in the future otherwise show tomorrows sunrise

---
+++ README.md
@@ -0,0 +1,7 @@
+WRITE IN PROPER MARKDOWN!!
+
+I would have chose a different api as it was hard to work with!
+
+Methods like getting the count down to sunset would have been made much easier with an date object than a string.
+
+Sortin gout the sunset time ended horrible, I usually like to use times which can be displayed and worked with using moment js as I find working with dates in javascript always gets a little ugly.
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ WeatherApiCall.prototype.call = function (callback) {
     self = this;
     $.getJSON(this.url, this.options).done(function (data) {
         // avoid having a data key with a value of an object called data
+        console.log(data);
         weather = _.pick(data.data, "current_condition", "weather");
         // make this in to a nice object to work with by picking what we want to use
 
@@ -38,14 +39,16 @@ WeatherApiCall.prototype.call = function (callback) {
                 // get the description which is very nested
                 _.pick(weather.current_condition[0].weatherDesc[0], "value"),
                 // find the sunset time
-                _.pick(weather.weather[0].astronomy[0], "sunset")
+                _.pick(weather.weather[0].astronomy[0], "sunset"),
+                // get tomorrows sunrise to show if the sun has set
+                _.pick(weather.weather[1].astronomy[0], "sunrise")
             );
         };
 
         function getForcast(arrayIndex) {
             return _.extend(
                 // find the sunset time
-                _.pick(weather.weather[arrayIndex].astronomy[0], "sunset"),
+                _.pick(weather.weather[arrayIndex].astronomy[0], "sunset", "sunrise"),
                 // pick from the conditions
                 _.pick(weather.weather[arrayIndex].hourly[0], "tempC", "windspeedMiles", "weatherCode"),
                 // get the description which is very nested
@@ -62,7 +65,38 @@ WeatherApiCall.prototype.setHtml = function () {
     $("#temperature").html(weather.temp_C || weather.tempC);
     $("#windSpeed").html(weather.windspeedMiles);
     $("#weatherCode").html(weather.weatherCode);
-    $("#sunset").html(weather.sunset);
+    $("#sunset").html(astromonyDetails(weather));
+
+    function astromonyDetails(weather) {
+
+        var sunsetTime, removePeriod, addTwelveHours, splitTime, parseMins, date, minutes, hour, seconds;
+        sunsetTime = weather.sunset;
+        removePeriod = sunsetTime.split(" ")[0];
+        splitTime = removePeriod.split(":");
+        addTwelveHours = ( parseInt(splitTime[0]) + 12 );
+        parseMins = parseInt(splitTime[1]);
+        //console.log(addTwelveHours);
+        date = new Date;
+        hour = date.getHours();
+        minutes = date.getMinutes();
+
+        // if the sun is setting later return sunsets at
+        if (addTwelveHours > hour) {
+            if (addTwelveHours === hour && parseMins > minutes) {
+                return "tomorrow sunrise is at "+weather.sunrise;
+            } else {
+                return "Sunsets at "+weather.sunset;
+            }
+        } else {
+            return "tomorrow sunrise is at "+weather.sunrise;
+        }
+        // or return tomorrows sunrise
+
+
+
+
+        return sunsetTime;
+    }
 
 
 };
